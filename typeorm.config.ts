9998fbc12536fac4dd164da5e69d1a02bc52f141
Julia Roldi <julialcantara.ra@gmail.com>
2023-10-01 14:18:30 -0300

    remove test

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -5,9 +5,9 @@ const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres', // Change this to match your database type
   host: 'localhost', // Your database host
   port: 5432, // Your database port
-  username: 'your_username', // Your database username
-  password: 'your_password', // Your database password
-  database: 'your_database_name', // Your database name
+  username: 'postgres', // Your database username
+  password: 'admin', // Your database password
+  database: 'care-connect-api-database', // Your database name
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true, // Set to true for development, but false for production
 };
