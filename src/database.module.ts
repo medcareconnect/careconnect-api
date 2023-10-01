b0aa5f24ae5ef3100432c9f3c87f7cc4c48f1bf9
Julia Roldi <julialcantara.ra@gmail.com>
2023-10-01 15:35:24 -0300

    congig env and workflow

--- src/database.module.ts
+++ src/database.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PatientsModule } from './patients/patients.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
@@ -11,16 +8,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     imports: [ConfigModule], 
     useFactory: (configService: ConfigService) => ({
       type: 'postgres' as 'postgres', // Change this to match your database type
-      host: configService.get<string>('PGHOST'), // Your database host
-      port: configService.get<number>('PGPORT'), // Your database port
-      username: configService.get<string>('PGUSER'), // Your database username
-      password: configService.get<string>('PGPASSWORD'), // Your database password
-      database: configService.get<string>('PGDATABASE'), // Your database name
+      host: configService.get<string>('AZURE_POSTGRESQL_HOST'), // Your database host
+      port: configService.get<number>('AZURE_POSTGRESQL_PORT'), // Your database port
+      username: configService.get<string>('AZURE_POSTGRESQL_USER'), // Your database username
+      password: configService.get<string>('AZURE_POSTGRESQL_PASSWORD'), // Your database password
+      database: configService.get<string>('AZURE_POSTGRESQL_DATABASE'), // Your database name
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: configService.get<boolean>('PGSYNCHRONIZE'), // Set to true for development, but false for production
     }),
-    inject:[ConfigService]
-    
+    inject:[ConfigService] 
   })],
 })
 export class DatabaseModule {}
